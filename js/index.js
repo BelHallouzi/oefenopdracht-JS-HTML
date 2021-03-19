3778cc5a8a52aa00d5b4467d40aa11b6629532a8
Bilal el Hallouzi <302562632@student.rocmondriaan.nl>
2021-03-19 18:07:31 +0100

    opdr. 1 t/m 3 gemaakt

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector(opinion);
+
+if(button) {
+    button.addEventListener("click", function() {
+        if(opinion){
+            document.getElementsByClassName("opinion")
+        }
+    })
+}
